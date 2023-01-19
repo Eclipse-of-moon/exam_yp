313ce1b6e61c1e9e802e750b95e87620e6a6d909
Abdula Magaramov <demer.chanel@gmail.com>
2023-01-19 17:31:24 +0300

    Part 2

--- Source_magaramov.cpp
+++ Source_magaramov.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 #include <algorithm>
+#include <cstdlib>
+#include <time.h>
 #include "game_magaramov.h"
-
-
-bool comp(Player& a, Player &b) {
-	return a.po < b.po;
-}
 
 int main()
 {
-	Game_Magaramov game(4);
+	int players_num;
+
+	srand(time(NULL));
+
+	std::cout << "Enter players num (2-5): ";
+	std::cin >> players_num;
+
+	if (players_num < 2 || players_num > 5) {
+		std::cout << "Invalid players num" << std::endl;
+		return 0;
+	}
+
+	Game_Magaramov game(players_num);
+
+	for (int i = 0; i < game.players.size(); i++) {
+		std::cout << "Enter player " << i + 1 << " name: ";
+		std::cin >> game.players[i].name;
+	}
 	
 	for (int year = 1; year <= 5; year++) {
 		game.phase1();
-		game.phase2();
+		game.phase246();
 		game.phase3();
-		game.phase4();
+		game.phase246();
 		game.phase5();
-		game.phase6();
+		game.phase246();
 		game.phase7();
 		game.phase8();
 	}
 
 	std::vector<Player> win_players;
-	int max_po = 0;
+	int max_po = INT_MIN;
 
 	for (int i = 0; i < game.players.size(); i++) {
 		if (game.players[i].po > max_po) {
@@ -37,9 +51,40 @@ int main()
 	}
 
 	std::vector<Player> players_copy = win_players;
-	int max_resource = win_players[0].resource.gold +
-		win_players[0].resource.tree +
-		win_players[0].resource.rock;
+	int max_resource = INT_MIN;
+
+	for (int i = 0; i < players_copy.size(); i++) {
+		int total = players_copy[i].resource.goods.total();
+
+		if (total > max_resource) {
+			win_players.clear();
+			win_players.push_back(players_copy[i]);
+			max_resource = total;
+		}
+		else if (total == max_resource) {
+			win_players.push_back(players_copy[i]);
+		}
+	}
+
+	players_copy = win_players;
+	int max_building = INT_MIN;
+
+	for (int i = 0; i < players_copy.size(); i++) {
+
+		if (players_copy[i].buildings.size() > max_building) {
+			win_players.clear();
+			win_players.push_back(players_copy[i]);
+			max_building = players_copy[i].buildings.size();
+		}
+		else if (players_copy[i].buildings.size() == max_building) {
+			win_players.push_back(players_copy[i]);
+		}
+	}
+
+	std::cout << "Win players:" << std::endl;
+	for (int i = 0; i < win_players.size(); i++) {
+		std::cout << "    " << win_players[i].name << std::endl;
+	}
 
 	return 0;
 }
--- game_magaramov.h
+++ game_magaramov.h
@@ -1,26 +1,56 @@
 #include <iostream>
 #include <vector>
 
-class Sovetnik {
+class Advisor {
 public:
 	std::string name;
 	std::string player_name;
+
 };
 
 class Building {
 public:
+	std::string name;
+};
+
+class Goods {
+public:
+	int gold;
+	int wood;
+	int stone;
+
+	int total() {
+		return gold + wood + stone;
+	}
+};
+
+class Cubic {
+public:
+	int min;
+	int max;
+	int value;
+
+	Cubic() {
+		min = 1;
+		max = 6;
+		value = min;
+	}
+
+	Cubic(int min_in, int max_in) {
+		min = min_in;
+		max = max_in;
+		value = min;
+	}
+
+	void roll();
 };
 
 class Resource {
 public:
-	int gold;
-	int tree;
-	int rock;
+	Goods goods;
 	int tokens;
-	int cubic1;
-	int cubic2;
-	int cubic3;
-	int cubic_white;
+	Cubic cubic[3];
+	Cubic cubic_white;
 	bool has_cubic_white;
 };
 
@@ -29,26 +59,45 @@ public:
 	std::string name;
 	Resource resource;
 	std::vector<Building> buildings;
-	Sovetnik *sovetnik;
+	Advisor advisor;
+	bool has_messenger;
 	int po;
 	int military;
+
+	void roll_cubices();
+	int total_cubices();
+};
+
+class Enemy {
+public:
+	std::string name;
+	int force;
+	int lost_po;
+	Goods lost_goods;
+	int got_po;
+	int got_goods;
 };
 
 class Game_Magaramov {
 public:
 	Game_Magaramov(int num_players);
 	void phase1();
-	void phase2();
 	void phase3();
-	void phase4();
 	void phase5();
-	void phase6();
 	void phase7();
 	void phase8();
+	void phase246();
 
 	std::vector<Player> players;
-	std::vector<Sovetnik> sovetniks;
+	std::vector<Advisor> advisors;
 	int year;
 	int phase;
-	int enemy;
+	Enemy enemy;
+
+private:
+	void phase246_bones();
+	void phase246_advisor();
+	void advisor_help(const char *advisor_name, Player &player);
+	void building(const char *advisor_name, Player &player);
+	void select_good(Player &player, int num);
 };
